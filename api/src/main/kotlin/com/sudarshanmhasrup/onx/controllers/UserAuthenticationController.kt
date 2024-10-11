bbd186b732308e300993025ee3b5d2b41e51bc2e
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-11 12:42:07 +0530

    Send custom response from /create-new-account endpoint

--- api/src/main/kotlin/com/sudarshanmhasrup/onx/controllers/UserAuthenticationController.kt
+++ api/src/main/kotlin/com/sudarshanmhasrup/onx/controllers/UserAuthenticationController.kt
@@ -1,5 +1,6 @@
 package com.sudarshanmhasrup.onx.controllers
 
+import com.sudarshanmhasrup.onx.domains.dto.CreateNewAccountResponseDTO
 import com.sudarshanmhasrup.onx.domains.dto.UserDTO
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -9,7 +10,11 @@ import org.springframework.web.bind.annotation.RestController
 class UserAuthenticationController {
 
     @PostMapping("/v1/auth/create-new-account")
-    fun createNewAccount(@RequestBody userDTO: UserDTO): UserDTO {
-        return userDTO
+    fun createNewAccount(@RequestBody userDTO: UserDTO): CreateNewAccountResponseDTO {
+        return CreateNewAccountResponseDTO(
+            statusCode = 200,
+            status = "Succeed!",
+            message = "New account created successfully with username: @${userDTO.username}!"
+        )
     }
 }
