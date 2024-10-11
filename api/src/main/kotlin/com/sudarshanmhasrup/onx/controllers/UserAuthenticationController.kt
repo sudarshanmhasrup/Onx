fec125da169c61fd6813c4296a9467549ed9a6af
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-11 12:10:25 +0530

    Setup user authentication controller

---
+++ api/src/main/kotlin/com/sudarshanmhasrup/onx/controllers/UserAuthenticationController.kt
@@ -0,0 +1,13 @@
+package com.sudarshanmhasrup.onx.controllers
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class UserAuthenticationController {
+
+    @PostMapping("/v1/auth/create-new-account")
+    fun createNewAccount() {
+
+    }
+}
