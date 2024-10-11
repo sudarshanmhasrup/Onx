72ec2f4c21e3ab61000b8a90c6fa1ffd6f5c92c8
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-11 16:32:42 +0530

    Change create account controller response

--- api/src/main/kotlin/com/sudarshanmhasrup/onx/controllers/UserAuthenticationController.kt
+++ api/src/main/kotlin/com/sudarshanmhasrup/onx/controllers/UserAuthenticationController.kt
@@ -4,6 +4,10 @@ import com.sudarshanmhasrup.onx.domains.dto.request.UserDTO
 import com.sudarshanmhasrup.onx.domains.dto.response.UserAccountCreationSuccessful
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.encodeToJsonElement
+import org.json.JSONObject
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -12,12 +16,17 @@ import org.springframework.web.bind.annotation.RestController
 class UserAuthenticationController {
 
     @PostMapping("/v1/auth/create-new-account")
-    fun createNewAccount(@RequestBody userDTO: UserDTO): String {
-        return Json.encodeToString(UserAccountCreationSuccessful(
+    fun createNewAccount(@RequestBody userDTO: UserDTO): JsonElement {
+
+        val response = UserAccountCreationSuccessful(
             status = "Succeed!",
             statusCode = 200,
-            response = "",
+            response = "A new account is created successfully with username: @${userDTO.username}!",
             data = userDTO
-        ))
+        )
+
+        val result = Json.encodeToJsonElement(response)
+
+        return result
     }
 }
