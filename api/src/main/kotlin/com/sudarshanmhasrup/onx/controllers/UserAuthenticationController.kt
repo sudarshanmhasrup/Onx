9d7725b1a7bb7f3220f512003b87701527eb4df5
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-11 12:35:03 +0530

    Modify controller settings

--- api/src/main/kotlin/com/sudarshanmhasrup/onx/controllers/UserAuthenticationController.kt
+++ api/src/main/kotlin/com/sudarshanmhasrup/onx/controllers/UserAuthenticationController.kt
@@ -1,5 +1,6 @@
 package com.sudarshanmhasrup.onx.controllers
 
+import com.sudarshanmhasrup.onx.domains.dto.UserDTO
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController
 class UserAuthenticationController {
 
     @PostMapping("/v1/auth/create-new-account")
-    fun createNewAccount() {
-
+    fun createNewAccount(@RequestBody userDTO: UserDTO): UserDTO {
+        return userDTO
     }
 }
--- api/src/main/kotlin/com/sudarshanmhasrup/onx/domains/dto/UserDTO.kt
+++ api/src/main/kotlin/com/sudarshanmhasrup/onx/domains/dto/UserDTO.kt
@@ -3,19 +3,19 @@ package com.sudarshanmhasrup.onx.domains.dto
 import java.sql.Date
 
 data class UserDTO(
-    val userId: String,
-    val username: String,
-    val firstName: String,
+    val userId: String = "",
+    val username: String = "",
+    val firstName: String = "",
     val lastName: String = "",
     val emailAddress: String = "",
     val mobileNumber: String = "",
-    val age: Int,
-    val gender: String,
-    val birthdate: Date,
-    val city: String,
-    val state: String,
-    val country: String,
-    val bio: String,
-    val profilePicture: String,
-    val accountCreationDate: Date
+    val age: Int = 0,
+    val gender: String = "",
+    val birthdate: String = "",
+    val city: String = "",
+    val state: String = "",
+    val country: String = "",
+    val bio: String = "",
+    val profilePicture: String = "",
+    val accountCreationDate: String = ""
 )
